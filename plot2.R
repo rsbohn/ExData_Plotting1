2d52392cc036c76364a491727367951d15ffcf14
Randall Bohn <rsbohn@familysearch.org>
2015-12-10 07:43:40 -0700

    completed plot 2

--- plot2.R
+++ plot2.R
@@ -13,7 +13,10 @@ Sys.setlocale(locale = "en_US.UTF-8")
 # http://stackoverflow.com/questions/28774111/creating-complicated-line-plots-in-r
 # Dates and Times in "R Programming for Data Science" R. Peng 2015-11-30
 
-with(DT, plot(timestamp, global_power, pch = 20, xlab = "", ylab = "Global Active Power (kilowatts)"))
+with(DT, plot(timestamp, global_power, xlab = "", 
+              ylab = "Global Active Power (kilowatts)",
+              type = "n"))
+lines(DT$timestamp, DT$global_power, lwd=2)
 
 dev.copy(png, file="plot2.png")
 dev.off()
